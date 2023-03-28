87467e59d03210840613350ba0448e0f5cabffc8
rafał żygadło <rafal@zygadlo.org>
2023-03-28 15:11:55 +0200

    vite config for server updated

--- www/html/resources/js/Pages/Users.vue
+++ www/html/resources/js/Pages/Users.vue
@@ -7,55 +7,39 @@ defineProps({
 });
 </script>
 
-<template>     
+<template>
 <AppLayout title="Dashboard">
     <template #header>
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
             Dashboard
         </h2>
     </template>
-
-    <table class="min-w-full text-left text-sm font-light">
+    <div class="relative sm:flex sm:justify-center sm:items-center min-h-screen selection:bg-red-500 selection:text-white">
+        <table class="min-w-full border divide-y divide-gray-200">
           <thead class="border-b font-medium dark:border-neutral-500">
             <tr>
               <th scope="col" class="px-6 py-4">#</th>
               <th scope="col" class="px-6 py-4">First</th>
-              <th scope="col" class="px-6 py-4">Last</th>
+              <th class="px-6 py-3 bg-gray-50">
+              <span class="text-xs font-medium leading-4 tracking-wider text-left text-gray-500 uppercase">Address</span>
+                </th>
               <th scope="col" class="px-6 py-4">Handle</th>
             </tr>
           </thead>
           <tbody>
-     <template v-for="user in users">
-        
-          
+         <template v-for="user in users">
             <tr class="border-b dark:border-neutral-500">
               <td class="whitespace-nowrap px-6 py-4 font-medium">1</td>
-              <td class="whitespace-nowrap px-6 py-4">Mark</td>
+              <td class="whitespace-nowrap px-6 py-4">{{user.name}}</td>
               <td class="whitespace-nowrap px-6 py-4">Otto</td>
               <td class="whitespace-nowrap px-6 py-4">@mdo</td>
             </tr>
-            <tr class="border-b dark:border-neutral-500">
-              <td class="whitespace-nowrap px-6 py-4 font-medium">2</td>
-              <td class="whitespace-nowrap px-6 py-4">Jacob</td>
-              <td class="whitespace-nowrap px-6 py-4">Thornton</td>
-              <td class="whitespace-nowrap px-6 py-4">@fat</td>
-            </tr>
-            <tr class="border-b dark:border-neutral-500">
-              <td class="whitespace-nowrap px-6 py-4 font-medium">3</td>
-              <td class="whitespace-nowrap px-6 py-4">Larry</td>
-              <td class="whitespace-nowrap px-6 py-4">Wild</td>
-              <td class="whitespace-nowrap px-6 py-4">@twitter</td>
-            </tr>
         </template>
-          
-        
-           
-       
-     
     </tbody>
-    
+
 </table>
-                    
+</div>
+
 
 </AppLayout>
 </template>
--- www/html/vite.config.js
+++ www/html/vite.config.js
@@ -3,6 +3,10 @@ import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
+    server: {
+        host: Object.values(networkInterfaces()).flat().find(i => i.family === 'IPv4' && !i.internal).address,
+        port: process.env.EXTERNAL_NODE_PORT,
+    },
     plugins: [
         laravel({
             input: 'resources/js/app.js',
